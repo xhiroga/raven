e6f5957e37d3f13717c96f2b59e374ab703e3c1e
Hiroaki Ogasawara <13391129+xhiroga@users.noreply.github.com>
2025-08-25 12:19:03 +0900

    fix: test

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import os
 import hydra
 from hydra.utils import instantiate
 from pytorch_lightning import Trainer, seed_everything
-from pytorch_lightning.plugins import DDPPlugin
+from pytorch_lightning.strategies import DDPStrategy
 import torch
 
 from data.data_module import DataModule
@@ -18,11 +18,16 @@ logging.getLogger("lightning").propagate = False
 
 @hydra.main(config_path="conf", config_name="config_test")
 def main(cfg):
+    print(f"{cfg=}")
     if cfg.fix_seed:
         seed_everything(42, workers=True)
 
-    print("The SLURM job ID for this run is {}".format(os.environ["SLURM_JOB_ID"]))
-    cfg.slurm_job_id = os.environ["SLURM_JOB_ID"]
+    if "SLURM_JOB_ID" in os.environ:
+        print("The SLURM job ID for this run is {}".format(os.environ["SLURM_JOB_ID"]))
+        cfg.slurm_job_id = os.environ["SLURM_JOB_ID"]
+    else:
+        print("Running locally (no SLURM)")
+        cfg.slurm_job_id = None
 
     cfg.gpus = torch.cuda.device_count()
     print("num gpus:", cfg.gpus)
@@ -37,7 +42,7 @@ def main(cfg):
     trainer = Trainer(
         **cfg.trainer,
         logger=wandb_logger,
-        strategy=DDPPlugin(find_unused_parameters=False) if cfg.gpus > 1 else None
+        strategy=DDPStrategy(find_unused_parameters=False) if cfg.gpus > 1 else "auto"
     )
 
     trainer.test(learner, datamodule=data_module)
